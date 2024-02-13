e804ba371e584f1dd6cc2e4841200a13d143cfa2
moaaz <moaz.elsayed002@gmail.com>
2024-02-13 22:46:26 +0200

    Switch database from Postgres to MySQL

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -7,8 +7,8 @@ export const AppDataSource = new DataSource({
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT
     ? parseInt(process.env.DATABASE_PORT, 10)
-    : 5432,
-  username: process.env.DATABASE_USERNAME,
+    : 3306,
+  username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
@@ -22,21 +22,21 @@ export const AppDataSource = new DataSource({
     migrationsDir: 'src/database/migrations',
     subscribersDir: 'subscriber',
   },
-  extra: {
-    // based on https://node-postgres.com/api/pool
-    // max connection pool size
-    max: process.env.DATABASE_MAX_CONNECTIONS
-      ? parseInt(process.env.DATABASE_MAX_CONNECTIONS, 10)
-      : 100,
-    ssl:
-      process.env.DATABASE_SSL_ENABLED === 'true'
-        ? {
-            rejectUnauthorized:
-              process.env.DATABASE_REJECT_UNAUTHORIZED === 'true',
-            ca: process.env.DATABASE_CA ?? undefined,
-            key: process.env.DATABASE_KEY ?? undefined,
-            cert: process.env.DATABASE_CERT ?? undefined,
-          }
-        : undefined,
-  },
+  // extra: {
+  //   // based on https://node-postgres.com/api/pool
+  //   // max connection pool size
+  //   max: process.env.DATABASE_MAX_CONNECTIONS
+  //     ? parseInt(process.env.DATABASE_MAX_CONNECTIONS, 10)
+  //     : 100,
+  //   ssl:
+  //     process.env.DATABASE_SSL_ENABLED === 'true'
+  //       ? {
+  //           rejectUnauthorized:
+  //             process.env.DATABASE_REJECT_UNAUTHORIZED === 'true',
+  //           ca: process.env.DATABASE_CA ?? undefined,
+  //           key: process.env.DATABASE_KEY ?? undefined,
+  //           cert: process.env.DATABASE_CERT ?? undefined,
+  //         }
+  //       : undefined,
+  // },
 } as DataSourceOptions);
