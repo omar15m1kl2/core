a71e915db50d255c76774da86748d1f9f04b4565
moaaz <moaz.elsayed002@gmail.com>
2024-03-05 02:37:17 +0200

    Remove commented out code and unused import in ChannelsService

--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -1,5 +1,4 @@
 import {
-  ForbiddenException,
   Injectable,
   NotFoundException,
   UnauthorizedException,
@@ -39,25 +38,6 @@ export class ChannelsService {
 
     return channel;
   }
-
-  // async getChannelMessages(
-  //   user: User,
-  //   id: Channel['id'],
-  //   paginationOptions: { cursor: number; limit: number },
-  // ) {
-  //   const channel = await this.channelRepostory.findOne({ id });
-  //   if (!channel) {
-  //     throw new NotFoundException();
-  //   }
-  //   if (channel.members.find((member) => member.id !== user.id)) {
-  //     throw new UnauthorizedException();
-  //   }
-
-  //   return await this.channelRepostory.findMessagesWithCursorPagination(
-  //     id,
-  //     paginationOptions,
-  //   );
-  // }
 
   async softDelete(user: User, id: Channel['id']): Promise<void> {
     const channel = await this.channelRepostory.findOne({ id });
