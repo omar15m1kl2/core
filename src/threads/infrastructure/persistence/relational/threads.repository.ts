142ad142355e6cf102388802ba93276337c6294d
Abdallah Elhadad <abdallah31009448@f-eng.tanta.edu.eg>
2024-03-07 20:08:24 +0200

    add abstract class

--- src/threads/infrastructure/persistence/relational/threads.repository.ts
+++ src/threads/infrastructure/persistence/relational/threads.repository.ts
@@ -1,1 +1,29 @@
-export abstract class ThreadRepository {}
+import { EntityCondition } from '../../../../utils/types/entity-condition.type';
+import { NullableType } from '../../../../utils/types/nullable.type';
+import { IPaginationOptions } from '../../../../utils/types/pagination-options';
+import { Thread } from '../../../domain/thread';
+
+export abstract class ThreadRepository {
+  abstract create(
+    data: Omit<
+      Thread,
+      | 'id'
+      | 'participants'
+      | 'parentMessage'
+      | 'createdAt'
+      | 'updatedAt'
+      | 'deletedAt'
+    >,
+  ): Promise<Thread>;
+
+  abstract findManyWithPagination(
+    userId: Thread['id'],
+    paginationOptions: IPaginationOptions,
+  ): Promise<Thread[]>;
+
+  abstract findOne(
+    fields: EntityCondition<Thread>,
+  ): Promise<NullableType<Thread>>;
+
+  abstract softDelete(id: Thread['id']): Promise<void>;
+}
