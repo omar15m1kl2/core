f4262861b12336270a53187d3558d211793059ee
Mohammed Zaher <mohammedzaherr0@gmail.com>
2024-04-16 22:55:40 +0200

    feat(invite): add unique constraint and an index on invite table

--- src/invites/infrastructure/persistence/entities/invite.entity.ts
+++ src/invites/infrastructure/persistence/entities/invite.entity.ts
@@ -7,14 +7,22 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  Index,
   ManyToOne,
   PrimaryGeneratedColumn,
+  Unique,
   UpdateDateColumn,
 } from 'typeorm';
 
 @Entity({
   name: 'invite',
 })
+@Unique('UQ_INVITEE_EMAIL_WORKSPACE_SENDER', [
+  'invitee_email',
+  'workspace',
+  'sender',
+])
+@Index('IDX_INVITEE_EMAIL', ['invitee_email', 'workspace'])
 export class InviteEntity extends EntityRelationalHelper implements Invite {
   @PrimaryGeneratedColumn()
   id: number;
