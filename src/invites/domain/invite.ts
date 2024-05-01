1114b0b2a2b77aa1d750d7247ea4c4594488eced
Moaaz Elsayed <65324805+moaazelsayed1@users.noreply.github.com>
2024-05-01 06:41:14 +0300

    fix(invite): return workspace title and image (#130)

--- src/invites/domain/invite.ts
+++ src/invites/domain/invite.ts
@@ -1,12 +1,13 @@
 import { InviteStatus } from 'src/inviteStatuses/domain/invite-status';
 import { User } from 'src/users/domain/user';
+import { DeepPartial } from 'src/utils/types/deep-partial.type';
 import { Workspace } from 'src/workspaces/domain/workspace';
 
 export class Invite {
   id: number | string;
   sender: User;
   invitee_email: string;
-  workspace: Workspace;
+  workspace: DeepPartial<Workspace>;
   status: InviteStatus;
   createdAt: Date;
   updatedAt: Date;
